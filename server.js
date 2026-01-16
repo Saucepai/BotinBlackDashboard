d6c33686702116f4685afb88e63364f63e8ef608
Saucepai <106503522+Saucepai@users.noreply.github.com>
2026-01-15 21:26:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
@@ -268,3 +269,4 @@ app.post('/login', async (req, res) => {
 
 }
 module.exports = { startDashboard };
+
